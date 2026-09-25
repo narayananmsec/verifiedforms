1cd7674a51fb23460165ccbe19eeb2665b96d2ee
narayananmsec <stationmusic82@gmail.com>
2026-09-25 20:17:47 +0530

    Remove Razorpay document field

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,6 @@ export interface Document {
   description: string;
   category: string;
   price: number;
-  razorpay_link: string;
   download_link: string;
   is_featured: boolean;
   download_count: number;
